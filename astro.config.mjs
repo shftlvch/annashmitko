80bc160a3dffbf8abff1b63a26460469a8a089ef
Daniil Sheftelevich <shftlvch@gmail.com>
2024-01-02 22:59:05 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://annashmitko.com/',
+    site: 'https://annashmitko.com/',
 });
