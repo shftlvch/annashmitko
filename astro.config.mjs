490c338cd9dcae771dab62f29537d7ecdb402bf9
Daniil Sheftelevich <shftlvch@gmail.com>
2025-10-16 09:34:49 +0300

    Upd fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -6,5 +6,20 @@ export default defineConfig({
   site: 'https://annashmitko.com/',
   vite: {
     plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Raleway",
+      cssVariable: "--font-raleway"
+    }, {
+      provider: fontProviders.google(),
+      name: "Roboto Serif",
+      cssVariable: "--font-roboto-serif"
+    }, {
+      provider: fontProviders.google(),
+      name: "Public Sans",
+      cssVariable: "--font-public-sans"
+    }]
   }
 });
