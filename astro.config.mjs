41e0586e86c0b51ed9402a0f5008d752fe4a181a
Daniil Sheftelevich <shftlvch@gmail.com>
2026-05-27 10:05:00 +0300

    add school page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,28 +9,26 @@ export default defineConfig({
   },
   site: 'https://annashmitko.com/',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  experimental: {
-    fonts: [{
-      // Default font for the CV page
-      provider: fontProviders.google(),
-      name: "Raleway",
-      cssVariable: "--font-raleway"
-    }, {
-      // Default serif font
-      provider: fontProviders.google(),
-      name: "Roboto Serif",
-      cssVariable: "--font-roboto-serif",
-      styles: ['normal', 'italic'],
-      weights: ["100 700"]
-    }, {
-      // Default sans font
-      provider: fontProviders.google(),
-      name: "Public Sans",
-      cssVariable: "--font-public-sans",
-      styles: ['normal'],
-      weights: ["400 700"]
-    }]
-  }
+  fonts: [{
+    // Default font for the CV page
+    provider: fontProviders.google(),
+    name: 'Raleway',
+    cssVariable: '--font-raleway',
+  }, {
+    // Default serif font
+    provider: fontProviders.google(),
+    name: 'Roboto Serif',
+    cssVariable: '--font-roboto-serif',
+    styles: ['normal', 'italic'],
+    weights: ['100 700'],
+  }, {
+    // Default sans font
+    provider: fontProviders.google(),
+    name: 'Public Sans',
+    cssVariable: '--font-public-sans',
+    styles: ['normal'],
+    weights: ['400 700'],
+  }],
 });
